51f7db2ac90bedfbcbf48946915bbd7d3fb77270
xingzhou-11 <1097480229@qq.com>
2020-10-24 20:26:53 -0700

    update

--- Src/OLED_SSD1306.c
+++ Src/OLED_SSD1306.c
@@ -2,20 +2,10 @@
 #include "ASCII_8X16.h"
 
 uint8_t Init_Data[] = {
-	0xAE, 0x00, 0x10, 0x40, 0xB0, 0x81, 0xA1, 0xA6, 
-	0xA8, 0x3F, 0xC8, 0xD3, 0x00, 0xD5, 0x80, 0xD9, 
-	0xDA, 0x12, 0xDB, 0x30,	0x8D, 0x10, 0xAF
+	0xAE, 0x00, 0x10, 0x40, 0xB0, 0x81, 0xA1, 0xA4,
+	0xA6, 0xA8, 0x3F, 0xC8, 0xD3, 0x00, 0xD5, 0x80, 
+	0xD9, 0xDA, 0x12, 0xDB, 0x30, 0x8D, 0x10, 0xAF
 	};
-
-//OLED初始化
-void OLED_Init()
-{	
-	for(uint8_t i=0; i<25; i++)
-	{
-		HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x00, I2C_MEMADD_SIZE_8BIT, Init_Data+i, 1, 0x100);
-		HAL_Delay(100);
-	}
-}
 
 //向OLED进行写命令操作
 void OLED_Write_Order(uint8_t Order)
@@ -29,18 +19,52 @@ void OLED_Write_Data(uint8_t Data)
 	HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x40, I2C_MEMADD_SIZE_8BIT, &Data, 1, 0x100);
 }
 
-//打开OLED
-void OLED_open()
+//OLED初始化
+void OLED_Init()
+{	
+	for(uint8_t i=0; i<24; i++)
+	{
+		HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x00, I2C_MEMADD_SIZE_8BIT, Init_Data+i, 1, 0x100);
+		HAL_Delay(100);
+	}
+}
+
+//清屏
+void OLED_Clear(void)
 {
+	for(uint8_t i=0; i<8; i++)
+	{
+		OLED_Write_Order(0xB0+i);
+		OLED_Write_Order(0x00);
+		OLED_Write_Order(0x10);
+		for (uint8_t j=0; j<128; j++)
+		{
+			OLED_Write_Data(0);
+		}
+		
+	}
+}
+
+//打开OLED
+//0x8D VCC电源设置
+//0x14 DCDC ON
+//0xA4 打开整个显示
+//0xAF 开启显示
+void OLED_open(void)
+{
+	OLED_Write_Order(0x8D);
+	OLED_Write_Order(0x14);
 	OLED_Write_Order(0xA4);
-	OLED_Write_Order(0xAE);
+	OLED_Write_Order(0xAF);
 }
 
 //关闭OLED
-void OLED_close()
+void OLED_close(void)
 {
+	OLED_Write_Order(0x8D);
+	OLED_Write_Order(0x10);
 	OLED_Write_Order(0xA5);
-	OLED_Write_Order(0xAF);
+	OLED_Write_Order(0xAE);
 }
 
 //指定位置显示字符
